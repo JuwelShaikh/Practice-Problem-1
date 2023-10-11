ec5b6a6edcdf530f04c74c5d252c0972ee23fce8
JuwelShaikh <juwelshaikh04@gmail.com>
2023-10-11 15:23:42 +0600

    Practice problem - es6

---
+++ practiceProblemOne.js
@@ -0,0 +1,15 @@
+const number = (a,b,c) => a * b * c;
+
+const result = number(10,20,30);
+console.log(result);
+
+const multyLines = `Hello
+My name is Juwel Shaikh
+I bought a new vape for myself!!`;
+
+console.log(multyLines);
+
+const arroFunc = (a, b = 10) => a+b;
+
+const result1 = arroFunc(10);
+console.log(result1);
